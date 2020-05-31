6bd3224665f278ee031b77a06a55ab068844bd13
Wayne Van Son <waynevanson@gmail.com>
2020-05-31 20:03:04 +1000

    fix: update exports to match updated files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * from "./dom-node";
 export * from "./query-selector";
 export * from "./query-selector-all";
 export * from "./contains";
+export * from "./util";
+export * from "./types";
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export * from "./event-names";
+export * from "./html-element-meta";
+export * from "./maps";
+export * from "./svg-element-meta";
