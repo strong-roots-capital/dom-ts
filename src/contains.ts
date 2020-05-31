69e336379c5d2600dade03387310cb1821334a71
Wayne Van Son <waynevanson@gmail.com>
2020-06-01 00:29:47 +1000

    fix: predicates and refinements are no longer Reader

--- src/contains.ts
+++ src/contains.ts
@@ -1,15 +1,12 @@
-import { reader, array } from "fp-ts";
+import { array } from "fp-ts";
 import { eqObjectIs } from "./util";
 
 /**
  * @summary
  * Asserts if the node is a descendant of the given ancestor.
  */
-export const isDescendant = <T extends Node = never>(
-  node: T
-): reader.Reader<Node, boolean> => (ancestor: Node) => ancestor.contains(node);
+export const isDescendant = <T extends Node = never>(node: T, ancestor: Node) =>
+  ancestor.contains(node);
 
-export const isChild = <T extends Node = never>(
-  node: T
-): reader.Reader<Node, boolean> => (parent) =>
+export const isChild = <T extends Node = never>(node: T, parent: Node) =>
   array.elem(eqObjectIs)(node, Array.from(parent.childNodes));
