d6244ddda8e7e9dad618325cfd9588b8a955c4aa
Wayne Van Son <waynevanson@gmail.com>
2020-09-18 03:35:53 +0000

    feat(document): adds createElement API

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
 export function fromIterable<T>(iterable: Iterable<T> | ArrayLike<T>): T[] {
   return Array.from(iterable)
 }
+
+export function instanceOf<A extends Function>(right: A) {
+  return <B>(left: B): left is Extract<B, A> => left instanceof right
+}
