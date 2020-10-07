cc8b5db4c5ffec7c3df5d5f2b34080c9f22647f9
Wayne Van Son <waynevanson@gmail.com>
2020-10-07 14:23:17 +0000

    style: removes unused import

--- src/reader-io-option.ts
+++ src/reader-io-option.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import { reader as R, readerT } from "fp-ts"
 import { IOOption as IOO } from "fp-ts-contrib"
 
@@ -12,8 +13,10 @@ const M = readerT.getReaderM(IOO.ioOption)
 
 export const ask: <R>() => ReaderIOOption<R, R> = M.ask
 export const asks: <R, A>(f: (r: R) => A) => ReaderIOOption<R, A> = M.asks
-export const fromIOOption: <R, A>(ma: IOO.IOOption<A>) => ReaderIOOption<R, A> = M.fromM
-export const fromReader: <R, A>(r: R.Reader<R, A>) => ReaderIOOption<R, A> = M.fromReader
+export const fromIOOption: <R, A>(ma: IOO.IOOption<A>) => ReaderIOOption<R, A> =
+  M.fromM
+export const fromReader: <R, A>(r: R.Reader<R, A>) => ReaderIOOption<R, A> =
+  M.fromReader
 
 export const ap = <R, A, B>(mab: ReaderIOOption<R, (a: A) => B>) => (
   fa: ReaderIOOption<R, A>
@@ -27,5 +30,6 @@ export const local = <R, A, Q>(f: (d: Q) => R) => (
   fa: ReaderIOOption<R, A>
 ): ReaderIOOption<Q, A> => M.local(fa, f)
 
-export const map = <A, B>(f: (a: A) => B) => <R>(fa: ReaderIOOption<R, A>): ReaderIOOption<R, B> =>
-  M.map(fa, f)
+export const map = <A, B>(f: (a: A) => B) => <R>(
+  fa: ReaderIOOption<R, A>
+): ReaderIOOption<R, B> => M.map(fa, f)
